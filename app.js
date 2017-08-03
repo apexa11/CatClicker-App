13a5ebc7b978d551649d2889351ab38999224e87
apexa11 <apexachoxi7@gmail.com>
2017-08-02 22:56:36 -0400

    update

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 
-var cats = $(".cat");
-var buttons = $("button");
+var model ={
+    currentCat : null;
+    cat : [
+        {
+            clickCounter : 0;
+            name : "pussy";
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568'
+        }
 
-function hideAllCats(){
-    for (var i=0; i<cats.length; i++){
-        $(cats[i]).hide();
-    }
+
+    ]
 }
-
-function bindButtonToCat(idNumber){
-    $("#button"+idNumber).click(function(){
-        hideAllCats();
-        $("#cat"+idNumber).show();
-    })
-}
-
-function bindCounterToCat(idNumber){
-    var cat = "#cat"+idNumber
-    $(cat).click(function(){
-        var count = $(cat+">.counter").text();
-        count = parseInt(count) + 1;
-        $(cat+" >.counter").text(count);
-    })
-}
-
-for (var i=1; i<=buttons.length; i++){
-    bindButtonToCat(i);
-}
-
-for (var i=1; i<=cats.length; i++){
-    bindCounterToCat(i);
-}
-
-hideAllCats();
-$("#cat1").show();
